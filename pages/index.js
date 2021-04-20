b936ae305b10876a492b07f1b13f2460d4266237
broklyn777 <50321354+broklyn777@users.noreply.github.com>
2021-04-20 10:03:34 +0200

    data content is up

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head'
+import { blogPosts } from '../lib/data';
 
 
 export default function Home() {
   return (
+  
     <div >
       <Head>
         <title>Create Next App</title>
@@ -13,8 +15,20 @@ export default function Home() {
         <h1 > My Blog </h1>
      
       </main>
+<div >
+  {blogPosts.map((item) => (
+    <div key="{item.title">
+      <div> {item.title} </div>
+      <div> {item.date.toString()} </div>
+      <div> {item.content} </div>
+    </div>
+  )
+  
+  
+  )}
+</div>
 
-   
+
     </div>
   );
 }
